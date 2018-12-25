62d76f1fd5e39f158f9b37fd790d3fc1c14905bd
Bhalani-Mauli <maulibhalani13@gmail.com>
2018-12-26 00:55:30 +0530

    forloop

---
+++ forloop/forloop.js
@@ -0,0 +1,18 @@
+console.log("-10 to 40")
+    for(var i=1;i<str.length;i++){
+        console.log(i);
+    }
+console.log("10 to 40")
+    for(let i=10;i<=40;i+=1){
+        console.log(i);
+    }
+console.log("300 to 333")
+    for(let i=300;i<=333;i++){
+        console.log(i)
+    }
+console.log("5 to 3")
+    for(let i=5;i<=50;i++){
+        if(i%5 === 0 && i%3 === 0){
+            console.log(i);
+        }
+    }
